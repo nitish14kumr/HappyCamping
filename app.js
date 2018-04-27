3df0c32283e5e95cc6b388570eb7cd2f0ddc0e11
Nitish Kumar <nitish14kumr@gmail.com>
2018-04-27 23:53:56 +0000

    add mongolab

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var campgroundRoutes    = require("./routes/campgrounds"),
     commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index");
     
-mongoose.connect("mongodb://localhost/yelp_camp");
+// mongoose.connect("mongodb://localhost/yelp_camp");
+mongoose.connect("mongodb://nitish:12345678@ds159509.mlab.com:59509/yelp_camp");
+
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
