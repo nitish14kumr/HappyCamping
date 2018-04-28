95e93fb705fc57129c0437cef739a758ff0b46cb
Nitish Kumar <nitish14kumr@gmail.com>
2018-04-28 00:11:49 +0000

    add env variable for databse

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var campgroundRoutes    = require("./routes/campgrounds"),
     commentRoutes       = require("./routes/comments"),
     indexRoutes         = require("./routes/index");
     
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://nitish:12345678@ds159509.mlab.com:59509/yelp_camp");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://nitish:12345678@ds159509.mlab.com:59509/yelp_camp");
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static(__dirname + "/public"));
